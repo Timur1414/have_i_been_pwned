cc32ccc605a7f179ae4b702858444d1769fe3ada
Timur <tima.temirov.2005@mail.ru>
2025-04-06 21:13:12 +0300

    Init.

---
+++ have_i_been_pwned/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for have_i_been_pwned project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'have_i_been_pwned.settings')
+
+application = get_asgi_application()
