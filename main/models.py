71803791ea5fdd2dfc1e1737ce27f272bc4cc3d8
Timur <tima.temirov.2005@mail.ru>
2025-04-06 23:25:33 +0300

    Added models.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,44 @@
+"""
+This file contains the models for the application.
+"""
+from django.contrib.auth.models import User
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Data(models.Model):
+    """
+    Abstract model for data that can be pwned.
+    """
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    pwned = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+class Email(Data):
+    """
+    Model for email data.
+    """
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+class Password(Data):
+    """
+    Model for password data.
+    """
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.password
+
+class Phone(Data):
+    """
+    Model for phone data.
+    """
+    phone = PhoneNumberField(unique=True, region='RU')
+
+    def __str__(self):
+        return self.phone
