68c9a88db501e45d729f68a2dc8825cbe6f3368a
Timur <tima.temirov.2005@mail.ru>
2025-04-07 13:40:25 +0300

    Added registration and authentication.

--- have_i_been_pwned/urls.py
+++ have_i_been_pwned/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.IndexPage.as_view(), name='index'),
+    
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('registration/', include('django_registration.backends.activation.urls')),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+class IndexPage(TemplateView):
+    """
+    A view that renders the index page.
+    """
+    template_name = 'index/index.html'
+
+    def get_context_data(self, **kwargs):
+        """
+        Adds context data to the template.
+        """
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Index'
+        return context
