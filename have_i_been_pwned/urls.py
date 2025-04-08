32af55ae3af4d8e03e55b4d20e5d8e6244ba2cbf
Timur <tima.temirov.2005@mail.ru>
2025-04-08 15:36:51 +0300

    Some css.

--- have_i_been_pwned/urls.py
+++ have_i_been_pwned/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path('registration/', include('django_registration.backends.activation.urls')),
+    path('profile/<int:id>/', views.ProfilePage.as_view(), name='profile'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, DetailView
+import logging
 
 def logout_view(request):
+    logging.info(f'{request.user.username} logged out')
     logout(request)
     return redirect('index')
 
@@ -20,4 +23,28 @@ class IndexPage(TemplateView):
         """
         context = super().get_context_data(**kwargs)
         context['title'] = 'Index'
+        return context
+
+
+class ProfilePage(DetailView, LoginRequiredMixin):
+    """
+    A view that renders the profile page.
+    """
+    template_name = 'profile/index.html'
+    context_object_name = 'user'
+
+    def get_object(self, queryset = ...):
+        """
+        Returns the user object for the profile page.
+        """
+        user = User.objects.get(id=self.kwargs['id'])
+        return user
+
+    def get_context_data(self, **kwargs):
+        """
+        Adds context data to the template.
+        """
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Profile'
+        logging.info(f'User {self.request.user.username} viewed profile of {self.object.username}')
         return context
